91e0b8892732c72d2bbe1d1e783008a70e43377f
hasan-yilmaz <75522218+hasan-yilmaz@users.noreply.github.com>
2021-05-02 22:18:20 +0300

    City Update oluşturuldu ve CityController tamamlandı.

--- Worker/Worker.UI/Controllers/CityController.cs
+++ Worker/Worker.UI/Controllers/CityController.cs
@@ -62,5 +62,42 @@ namespace Worker.UI.Controllers
 
             return RedirectToAction("Index", "City");
         }
+
+        [HttpGet]
+        public ActionResult Update(int id = 0)
+        {
+            City city = cityOperation.GetById(id);
+            CityCRUDModel cityCRUDModel = new CityCRUDModel();
+
+            if (city != null)
+            {
+                List<City> cityList = cityOperation.GetAllCity();
+                cityCRUDModel.CityList = new SelectList(cityList, "CityId", "CityName");
+
+                cityCRUDModel.CityId = city.CityId;
+                cityCRUDModel.CityName = city.CityName;
+            }
+            else
+            {
+                return RedirectToAction("Index", "City");
+            }
+            return View(cityCRUDModel);
+        }
+
+        [HttpPost]
+        public ActionResult Update(CityCRUDModel model)
+        {
+            City city = cityOperation.GetById(model.CityId);
+
+            if (city != null)
+            {
+                city.CityId = model.CityId;
+                city.CityName = model.CityName;
+
+                cityOperation.Update(city);
+            }
+
+            return RedirectToAction("Index", "City");
+        }
     }
 }
