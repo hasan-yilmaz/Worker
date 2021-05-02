2ae328b59d2d9c20810a28f009b4eeb32e8a47ab
hasan-yilmaz <75522218+hasan-yilmaz@users.noreply.github.com>
2021-05-02 23:49:31 +0300

    HobbyController oluşturuldu ve Index eklendi.

---
+++ Worker/Worker.UI/Controllers/HobbyController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Worker.BIZ;
+using Worker.DATA;
+using Worker.UI.Models;
+
+namespace Worker.UI.Controllers
+{
+    public class HobbyController : Controller
+    {
+        DBWorkerEntities db;
+        HobbyOperation hobbyOperation;
+
+        public HobbyController()
+        {
+            db = new DBWorkerEntities();
+            hobbyOperation = new HobbyOperation(db);
+        }
+
+        public ActionResult Index()
+        {
+            List<HobbyListViewModel> hobbyListViewModelList = new List<HobbyListViewModel>();
+
+            List<Hobby> hobbyList = hobbyOperation.GetAllHobby();
+
+            foreach (Hobby hobby in hobbyList)
+            {
+                hobbyListViewModelList.Add(new HobbyListViewModel()
+                {
+                    HobbyId = hobby.HobbyId,
+                    Name = hobby.HobbyName,
+                    IsActive = hobby.IsActive
+                });
+            }
+            return View(hobbyListViewModelList);
+        }
+    }
+}
