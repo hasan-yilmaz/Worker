1b16093f42df40039d2f20a0457e26ba3003b485
hasan-yilmaz <75522218+hasan-yilmaz@users.noreply.github.com>
2021-05-03 00:46:18 +0300

    Hobby Update oluşturuldu ve HobbyController tamamlandı.

--- Worker/Worker.UI/Controllers/HobbyController.cs
+++ Worker/Worker.UI/Controllers/HobbyController.cs
@@ -37,5 +37,69 @@ namespace Worker.UI.Controllers
             }
             return View(hobbyListViewModelList);
         }
+
+        [HttpGet]
+        public ActionResult Insert()
+        {
+            HobbyCRUDModel hobbyCRUDModel = new HobbyCRUDModel();
+
+            List<Hobby> hobbyList = hobbyOperation.GetAllHobby();
+            hobbyCRUDModel.HobbyList = new SelectList(hobbyList, "HobbyId", "HobbyName");
+
+            return View(hobbyCRUDModel);
+        }
+
+        [HttpPost]
+        public ActionResult Insert(HobbyCRUDModel model)
+        {
+            Hobby hobby = new Hobby()
+            {
+                HobbyId = model.HobbyId,
+                HobbyName = model.HobbyName,
+                IsActive = true
+            };
+            hobbyOperation.Insert(hobby);
+
+            return RedirectToAction("Index","Hobby");
+        }
+
+        [HttpGet]
+        public ActionResult Update(int id = 0)
+        {
+            HobbyCRUDModel hobbyCRUDModel = new HobbyCRUDModel();
+            Hobby hobby = hobbyOperation.GetById(id);
+
+            if (hobby != null)
+            {
+                List<Hobby> hobbyList = hobbyOperation.GetAllHobby();
+                hobbyCRUDModel.HobbyList = new SelectList(hobbyList, "HobbyId", "HobbyName");
+
+                hobbyCRUDModel.HobbyId = hobby.HobbyId;
+                hobbyCRUDModel.HobbyName = hobby.HobbyName;
+            }
+            else
+            {
+                return RedirectToAction("Index", "Hobby");
+            }
+
+            return View(hobbyCRUDModel);
+        }
+
+        [HttpPost]
+        public ActionResult Update(HobbyCRUDModel model)
+        {
+            Hobby hobby = hobbyOperation.GetById(model.HobbyId);
+
+            if (hobby != null)
+            {
+                hobby.HobbyId = model.HobbyId;
+                hobby.HobbyName = model.HobbyName;
+
+                hobbyOperation.Update(hobby);
+            }
+
+
+            return RedirectToAction("Index","Hobby");
+        }
     }
 }
